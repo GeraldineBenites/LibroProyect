548a2c74228be19802e23b11f8425470962b3210
Geraldine Benites <g30a06b01@gmail.com>
2024-05-17 11:29:35 -0500

    Cambio realizado en LibroController.java

--- src/main/java/com/tuuniversidad/controllers/LibroController.java
+++ src/main/java/com/tuuniversidad/controllers/LibroController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tuuniversidad.models.Libro;
 import com.tuuniversidad.service.LibroService;
 
+@RestController
 public class LibroController {
 
     @Autowired
