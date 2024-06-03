fe91cdb5852479ab8e3d599795af410c87920cba
Geraldine Benites <g30a06b01@gmail.com>
2024-06-03 08:58:12 -0500

    modificacion por error

--- src/main/java/com/tuuniversidad/utils/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/tuuniversidad/utils/RestResponseEntityExceptionHandler.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @RestController
-public class RestResponseEntityExceptionHandler {
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+   
     @ExceptionHandler(LibroException.class)
     public ResponseEntity<ExceptionResponse> toNotFound(LibroException ex){
         ExceptionResponse response = new ExceptionResponse("Error en Libro","Any details");
